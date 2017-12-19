67032b5a2a0d5f8dc042f99e7588422a3c9aaf97
jhotadhari <happyhomelesshippie@allcyclistsarebeautiful.com>
2017-12-19 14:15:55 +0530

    bug fix in addPkg, loading script _integration_cmb2_qtranslate_init

--- generators/addPkg/templates/src/inc/dep/autoload/_cmb2_taxonomy_init.php
+++ generators/addPkg/templates/src/inc/dep/autoload/_cmb2_taxonomy_init.php
@@ -5,10 +5,10 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// _cmb2_taxonomy_init
+// <%= funcPrefix %>_cmb2_taxonomy_init
 function <%= funcPrefix %>_cmb2_taxonomy_init() {
 	if (! class_exists('CMB2_Taxonomy')) {
-		include_once WP_PLUGIN_DIR . '/<%= pluginSlug %>/' . 'vendor/jcchavezs/cmb2-taxonomy/init.php';
+		include_once WP_PLUGIN_DIR . '/<%= pluginSlug %>' . '/vendor/jcchavezs/cmb2-taxonomy/init.php';
 	}
 }
 add_action('admin_init', '<%= funcPrefix %>_cmb2_taxonomy_init', 3);
--- generators/addPkg/templates/src/inc/dep/autoload/_integration_cmb2_qtranslate_init.php
+++ generators/addPkg/templates/src/inc/dep/autoload/_integration_cmb2_qtranslate_init.php
@@ -5,11 +5,9 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// _integration_cmb2_qtranslate_init
+// <%= funcPrefix %>_integration_cmb2_qtranslate_init
 function <%= funcPrefix %>_integration_cmb2_qtranslate_init() {
-		
-	wp_register_script('cmb2_qtranslate_main', WP_PLUGIN_URL . '<%= pluginSlug %>' . '/vendor/jmarceli/integration-cmb2-qtranslate/dist/scripts/main.js', array('jquery'));
-	wp_enqueue_script('cmb2_qtranslate_main');
+	wp_enqueue_script('cmb2_qtranslate_main', WP_PLUGIN_URL . '/<%= pluginSlug %>' . '/vendor/jmarceli/integration-cmb2-qtranslate/dist/scripts/main.js', array('jquery'));
 }
 add_action('admin_enqueue_scripts', '<%= funcPrefix %>_integration_cmb2_qtranslate_init');
 
